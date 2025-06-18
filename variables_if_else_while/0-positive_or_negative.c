495e3a2ad21843b2a16876588ac7fe5cf290946d
root <carlaciarr@gmail.com>
2025-06-18 07:26:11 +0000

    description

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -5,6 +5,12 @@
 /**
  * main - Entry point
  *
+ * Description: This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print whether the number stored in the variable n is positive or negative.
+ * The output of the program should be the number, followed by:
+ * - if the number is greater than 0: is positive
+ * - if the number is 0: is zero
+ * - if the number is less than 0: is negative
+ *
  * Return: Always 0 (Success)
  */
 
