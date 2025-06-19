84b4b64532e459d7514be935ce877e442ea9a3c0
root <carlaciarr@gmail.com>
2025-06-19 07:56:26 +0000

    add 7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 57;
+
+	while (n <= 48)
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}
