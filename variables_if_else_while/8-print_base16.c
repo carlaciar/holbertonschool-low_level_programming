5ea4a52fa147b34d3452d87d2486835929ddbb76
root <carlaciarr@gmail.com>
2025-06-19 13:48:46 +0000

    add 8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char number = 48;
+	char letter = 'a';
+
+	while (number <= 57)
+	{
+		putchar(number);
+		number++;
+	}
+	while (letter <= 'F')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
