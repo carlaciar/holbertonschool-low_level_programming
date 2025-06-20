8f301c815e623b56f1066eb442563e676b44c3b8
root <carlaciarr@gmail.com>
2025-06-20 13:27:10 +0000

    add 3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - lowercase
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return 1;
+	else
+		return 0;
+}
--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -3,22 +3,23 @@
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - check the code.
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**prints the alphabet in lowercase
+ * Return: Always 0.
  */
 int main(void)
 {
+	int r;
+
     print_alphabet();
     print_alphabet_x10();
+
+    r = _islower('H');
+    _putchar(r + '0');
+    r = _islower('o');
+    _putchar(r + '0');
+    r = _islower(108);
+    _putchar(r + '0');
+    _putchar('\n');
     return (0);
 }
