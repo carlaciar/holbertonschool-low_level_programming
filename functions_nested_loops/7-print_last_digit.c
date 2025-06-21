526a5cf39b6baa96e5282e329a4006b63eff4a5a
root <carlaciarr@gmail.com>
2025-06-21 04:16:03 +0000

    edit 7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -11,6 +11,8 @@ int print_last_digit(int n)
 	int last_digit = n % 10;
 
 		if (last_digit < 0)
-			return (-n);
-	return (n);
+			last_digit = -last_digit;
+
+		_putchar('0' + last_digit);
+		return (last_digit);
 }
