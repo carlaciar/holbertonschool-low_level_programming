a3252ecb65ea72bff76f892cbee5e264951e12ef
root <carlaciarr@gmail.com>
2025-06-21 03:55:39 +0000

    add 6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value of an integer.
+ * @n: the number
+ *
+ * Return: -n negative number, n flip to positive
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	return (n);
+}
