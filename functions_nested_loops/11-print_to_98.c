924c089228b04ce56cdfcf6405c18be487baba5e
root <carlaciarr@gmail.com>
2025-06-21 05:50:43 +0000

    edit 11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,16 +9,44 @@
  */
 void print_to_98(int n)
 {
-	while (n != 98)
+	int current = n;
+
+	while (current != 98)
 	{
-		_putchar(n);
+		int num = current;
+
+		if (num < 0)
+		{
+			_putchar('-');
+			num = -num;
+		}
+
+		if (num >= 100)
+		{
+			_putchar((num / 100) + '0');
+			_putchar((num / 10 % 10) + '0');
+			_putchar((num % 10) + '0');
+		}
+		else if (num >= 10)
+		{
+			_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
+		}
+		else
+		{
+			_putchar(num + '0');
+		}
+
 		_putchar(',');
 		_putchar(' ');
-		if (n < 98)
-			n++;
+
+		if (current < 98)
+			current++;
 		else
-			n--;
+			current--;
 	}
-	_putchar(98);
+
+	_putchar('9');
+	_putchar('8');
 	_putchar('\n');
 }
