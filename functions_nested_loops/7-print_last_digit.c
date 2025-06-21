556074870c18ebc9e440cf25ad78265d7e9e71a2
root <carlaciarr@gmail.com>
2025-06-21 04:04:41 +0000

    add 7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - checks for alphabetic character.
+ * @n: the number
+ *
+ * Return: 0 is number
+ */
+int print_last_digit(int n)
+{
+	_putchar(n % 10);
+	return (0);
+}
