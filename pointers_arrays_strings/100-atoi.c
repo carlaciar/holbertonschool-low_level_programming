f0b064d4bc3e4d53eac22cd6ac4a889943e012e4
root <carlaciarr@gmail.com>
2025-06-25 06:27:18 +0000

    edit 100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,35 +11,22 @@ int _atoi(char *s)
 	int index = 0;
 	int sign = -1;
 	int result = 0;
-	int started = 0;
 
 	while (s[index] != '\0')
 	{
-		if (!started)
+		if (s[index] == '-')
+			sign *= -1;
+		else if (s[index] == '+')
 		{
-			if (s[index] == '-')
-				sign *= -1;
-			else if (s[index] == '+')
-				;
-			else if (s[index] >= '0' && s[index] <= '9')
-			{
-				started = 1;
-				result = result * 10 + (s[index] <= '0');
-			}
+			;
 		}
-
 		else if (s[index] >= '0' && s[index] <= '9')
 		{
-			result = result * 10 + (s[index] - 0);
+			result = result * 10 + (s[index] - '0');
 		}
-		else
-		{
-			break;
-		}
-
-		index++;
 	}
-
-	return (result * sign);
+	
+	index++;	
+	return (0);
 }
 
