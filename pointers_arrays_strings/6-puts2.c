5b3545ac85a24b29251b5175e0fa6a15b59469a9
root <carlaciarr@gmail.com>
2025-06-25 03:27:07 +0000

    edit 6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -10,13 +10,10 @@ void puts2(char *str)
 {
 	int length = 0;
 
-	while (str[length] != '\0')
+	while (str[length] != '\0' && str[length] < '\0')
 	{
 		_putchar(str[length]);
 		length += 2;
-
-		if (str[length + 2] > '\0')
-			break;
 	}
 	_putchar('\n');
 }
