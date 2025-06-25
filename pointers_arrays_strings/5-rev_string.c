5dbabd3d1c222441123985b9f7fb760478c0cf3d
root <carlaciarr@gmail.com>
2025-06-25 02:50:34 +0000

    5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - function that reverses a string
+ * @s: the string
+ *
+ */
+void rev_string(char *s)
+{
+	int length = 0;
+	int start = 0;
+        int end = length - 1;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	while (start < end)
+	{
+		char temp = s[start];
+
+		s[start] = s[end];
+		s[end] = temp;
+		start++;
+		end--;
+	}
+}
+
+
