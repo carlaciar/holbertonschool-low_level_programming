6c5220f9d73daeb1e693a834c60a97af58f0cb7f
root <carlaciarr@gmail.com>
2025-06-27 06:40:55 +0000

    no ternary operation

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -9,22 +9,24 @@
  */
 char *leet(char *str)
 {
-	int index = 0;
+	int index = 0, position;
+	char from[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char to[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
 	while (str[index] != '\0')
 	{
-		if (str[index] == 'a' || str[index] == 'A' || str[index] == 'e' ||
-				str[index] == 'E' || str[index] == 'o' ||
-				str[index] == 'O' || str[index] == 't' ||
-				str[index] == 'T' || str[index] == 'l' ||
-				str[index] == 'L')
+		position = 0;
+
+		while (position < 10)
 		{
-			str[index] = (str[index] == 'a' || str[index] == 'A') ? '4' :
-				(str[index] == 'e' || str[index] == 'E') ? '3' :
-				(str[index] == 'o' || str[index] == 'O') ? '0' :
-				(str[index] == 't' || str[index] == 'T') ? '7' :
-				(str[index] == 'l' || str[index] == 'L') ? '1' :
-				str[index];
+			if (str[index] == from[position])
+			{
+				str[index] = to[position];
+				break;
+			}
+
+			position++;
+
 		}
 
 		index++;
@@ -34,3 +36,5 @@ char *leet(char *str)
 	return (str);
 
 }
+
+
