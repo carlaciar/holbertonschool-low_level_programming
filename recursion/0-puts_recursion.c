5299395d67200628d1bdbcc0905e1221315af04c
root <carlaciarr@gmail.com>
2025-06-30 10:38:29 +0000

    add 0-puts_recursion.c

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string, followed by a new line
+ * @s: string
+ *
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	_putchar('\n');
+}
