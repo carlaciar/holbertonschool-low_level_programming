7721aa5bd7f5627fa7f8ffffe34c27e44adf333e
root <carlaciarr@gmail.com>
2025-07-07 09:21:27 +0000

    add dog.h

---
+++ structures_typedef/README.md
@@ -0,0 +1,1 @@
+# Week 6: C - Structures, typedef
---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog types
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
