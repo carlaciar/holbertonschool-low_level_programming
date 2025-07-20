b3ae32c3c3d08e93d99d81cf1b3e7a34e28f0511
root <carlaciarr@gmail.com>
2025-07-20 07:42:57 +0000

    edit 3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -8,37 +8,37 @@
  */
 void print_all(const char * const format, ...)
 {
+	char *str, *sep = "";
 	va_list args;
-	unsigned int i = 0, j = 0;
-	char *str;
-	char *sep = "";
-	char valid[] = "cifs";
+	int i = 0;
 
 	va_start(args, format);
 
 	while (format && format[i])
 	{
-		while (valid[j])
+		switch (format[i])
 		{
-			if (format[i] == valid[j])
-			{
-				printf("%s", sep);
-				if (format[i] == 'c')
-					printf("%c", va_arg(args, int));
-				else if (format[i] == 'i')
-					printf("%d", va_arg(args, int));
-				else if (format[i] == 'f')
-					printf("%f", va_arg(args, double));
-				else if (format[i] == 's')
-				{
-					str = va_arg(args, char *);
-					printf("%s", str ? str : "(nil)");
-				}
-				sep = ", ";
-			}
-			j++;
+			case 'c':
+				printf("%s%c", sep, va_arg(args, int));
+				break;
+				case 'i':
+				printf("%s%d", sep, va_arg(args, int));
+				break;
+				case 'f':
+				printf("%s%f", sep, va_arg(args, double));
+				break;
+				case 's':
+				str = va_arg(args, char *);
+				if (!str)
+					str = "(nil)";
+				printf("%s%s", sep, str);
+				break;
 		}
-		j = 0;
+		if (format[i] == 'c' ||
+				format[i] == 'i' ||
+				format[i] == 'f' ||
+				format[i] == 's')
+			sep = ", ";
 		i++;
 	}
 
