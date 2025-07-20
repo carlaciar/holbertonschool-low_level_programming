079ee88cbccfe844f3104c26a1a5326b8366be78
root <carlaciarr@gmail.com>
2025-07-20 05:23:24 +0000

    add 0-print_name.c

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - function that prints a name
+ * @name: name being printed
+ *
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+        f(name);
+}
---
+++ function_pointers/README.md
@@ -0,0 +1,2 @@
+# Week 7: C - Function pointers
+
