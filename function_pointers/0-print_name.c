e6052551be4636f0e9fd112c05ecb82fa880b458
root <carlaciarr@gmail.com>
2025-07-20 05:25:57 +0000

    add @f description

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -5,11 +5,12 @@
 /**
  * print_name - function that prints a name
  * @name: name being printed
+ * @f: pointer to the function
  *
  * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
-        f(name);
+		f(name);
 }
