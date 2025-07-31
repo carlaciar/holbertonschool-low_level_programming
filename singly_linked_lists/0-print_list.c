56f1e463cc4d14502da87c70de41ba119697e22b
root <carlaciarr@gmail.com>
2025-07-31 12:25:03 +0000

    betty doc

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- * size_t print_list - a function that prints all the elements of a list_t list
+ * print_list - a function that prints all the elements of a list_t list
  * @h: the string
  *
  * Return: the number of nodes
