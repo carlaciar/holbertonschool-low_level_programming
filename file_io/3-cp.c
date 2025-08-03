320b76bc5fbd679128f0fcbd15636917db08c807
root <carlaciarr@gmail.com>
2025-08-03 14:19:54 +0000

    edit

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -59,10 +59,12 @@ int main(int argc, char *argv[])
 	}
 
 	if (close(fd_from) == -1)
-		print_error(100, "Error: Can't close fd %d\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from),
+			exit(100);
 
 	if (close(fd_to) == -1)
-		print_error(100, "Error: Can't close fd %d\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to),
+			exit(100);
 
 	return (0);
 }
