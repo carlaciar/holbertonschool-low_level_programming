8219716021eea504563e58e918647ae45a230e12
root <carlaciarr@gmail.com>
2025-08-26 04:47:08 +0000

    fix STDOUT_FILENO

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -16,31 +16,38 @@ int main(int argc, char *argv[])
 	char buffer[1024];
 
 	if (argc != 3)
-		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
+		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n"),
+			exit(97);
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n",
+				argv[1]), exit(98);
 
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
-		close(fd_from), dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+		close(fd_from), dprintf(STDOUT_FILENO,
+				"Error: Can't write to %s\n", argv[2]), exit(99);
 
 	while ((rd = read(fd_from, buffer, 1024)) > 0)
 	{
 		wr = write(fd_to, buffer, rd);
 		if (wr != rd)
 			close(fd_from), close(fd_to),
-			dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+			dprintf(STDOUT_FILENO, "Error: Can't write to %s\n",
+					argv[2]), exit(99);
 	}
 	if (rd == -1)
 		close(fd_from), close(fd_to),
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n",
+				argv[1]), exit(98);
 
 	if (close(fd_from) == -1)
-		dprintf(2, "Error: Can't close fd %d\n", fd_from), exit(100);
+		dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n",
+				fd_from), exit(100);
 	if (close(fd_to) == -1)
-		dprintf(2, "Error: Can't close fd %d\n", fd_to), exit(100);
+		dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", fd_to),
+			exit(100);
 
 	return (0);
 }
