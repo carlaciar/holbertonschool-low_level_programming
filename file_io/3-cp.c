1c47ceaa6c7f7483c7838a96c68c3497a54af250
root <carlaciarr@gmail.com>
2025-08-26 05:25:07 +0000

    edit

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -41,54 +41,46 @@ static void must_close(int fd)
 }
 
 /**
- * main - copy file_from to file_to
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 on success, exits 97-100 on errors
+ * copy_file - copy file_from to file_to
+ * @from: source filename
+ * @to: destination filename
  */
-int main(int argc, char **argv)
+static void copy_file(char *from, char *to)
 {
-	int fd_from;
-	int fd_to;
+	int fd_from, fd_to;
 	ssize_t rd;
 	char buf[1024];
 
-	if (argc != 3)
-	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
-	fd_from = open(argv[1], O_RDONLY);
+	fd_from = open(from, O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(1, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(1, "Error: Can't read from file %s\n", from);
 		exit(98);
 	}
 
 	rd = read(fd_from, buf, 1024);
 	if (rd == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(2, "Error: Can't read from file %s\n", from);
 		must_close(fd_from);
 		exit(98);
 	}
 
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	fd_to = open(to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		dprintf(2, "Error: Can't write to %s\n", to);
 		must_close(fd_from);
 		exit(99);
 	}
 
 	while (rd > 0)
 	{
-		write_all(fd_to, buf, rd, argv[2]);
+		write_all(fd_to, buf, rd, to);
 		rd = read(fd_from, buf, 1024);
 		if (rd == -1)
 		{
-			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+			dprintf(2, "Error: Can't read from file %s\n", from);
 			must_close(fd_from);
 			must_close(fd_to);
 			exit(98);
@@ -97,6 +89,23 @@ int main(int argc, char **argv)
 
 	must_close(fd_from);
 	must_close(fd_to);
+}
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, exits 97-100 on errors
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	copy_file(argv[1], argv[2]);
 	return (0);
 }
 
